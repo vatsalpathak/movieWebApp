c16c9cc6165e69a19e00a5e94845c3eb93fc46ab
Vatsal Pathak <vatsal.pathak@dskservices.com>
2023-09-14 14:56:02 -0700

    Login Form

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import NotFound from "./components/notFound";
 import Rentals from "./components/rentals";
 import Customers from "./components/customer";
 import NavBar from "./components/navBar";
-import "./App.css";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/not-found" component={NotFound} />
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,1 +1,92 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
+import Input from "./common/input";
+
+class LoginForm extends Component {
+  username = React.createRef();
+
+  state = {
+    account: {
+      username: "",
+      password: "",
+    },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
+  // This is one way to call Reference to pass values in React
+  //   componentDidMount() {
+  //     this.username.current.focus();
+  //   }
+  validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
+
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
+    //Call the server
+    console.log("Submitted");
+  };
+
+  validateProperty = ({ name, value }) => {
+    // computed prorperties are used by using "[]"
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account, errors });
+  };
+  render() {
+    const { account, errors } = this.state;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+            error={errors.username}
+          />
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+            error={errors.password}
+          />
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
